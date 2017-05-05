bf4adf55f2fbf85c51526caa9b78df43ea57ac5b
JeffreyMilner <j.milner359@gmail.com>
2017-05-05 14:52:15 -0700

    quick change

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 const chalk = require('chalk');
 module.exports = client => {
-  console.log(chalk.bgGreen.black('I\'m Online\nI\'m Online'));
+  console.log(chalk.bgGreen.black('I\'m Online'));
 };
