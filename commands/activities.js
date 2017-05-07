c62f8d155793b8b88e2322ad3108df8f96ee6bba
JeffreyMilner <j.milner359@gmail.com>
2017-05-06 19:25:04 -0700

    Fixed a few things

--- commands/activities.js
+++ commands/activities.js
@@ -4,37 +4,37 @@ exports.run = (client, message, args) => {
     switch (day) {
         case 'sun':
         case 'sunday':
-            message.channel.sendCode('asciidoc', `== Before Reset: == \nComplete Arena Battles (5) \nSave Cantina Energy \nSave Normal Energy
+            message.channel.sendCode('asciidoc', `== Before Reset == \nComplete Arena Battles \nSave Cantina Energy \nSave Normal Energy
                                  \n== After Reset == \nSpend Cantina Energy \nSave Normal Energy`);
             break;
         case 'mon':
         case 'monday':
-            message.channel.sendCode('asciidoc', `== Before Reset: == \nSpend Cantina Energy \nSave Normal Energy \nSave Galactic War (unless reset available)
+            message.channel.sendCode('asciidoc', `== Before Reset == \nSpend Cantina Energy \nSave Normal Energy \nSave Galactic War (unless reset available)
                                  \n== After Reset == \nSpend Normal Energy on Light Side Battles \nSave Galactic War (unless reset available)`);
             break;
         case 'tue':
         case 'tuesday':
-            message.channel.sendCode('asciidoc', `== Before Reset: == \nSpend Normal Energy on Light Side Battles \nSave Galactic War
-                                 \n== After Reset == \nComplete Galactic War Battles (24 with restart) \nSave Normal Energy`);
+            message.channel.sendCode('asciidoc', `== Before Reset == \nSpend Normal Energy on Light Side Battles \nSave Galactic War
+                                 \n== After Reset == \nComplete Galactic War Battles \nSave Normal Energy`);
             break;
         case 'wed':
         case 'wednesday':
-            message.channel.sendCode('asciidoc', `== Before Reset: == \nComplete Galactic War Battles \nSave Normal Energy
+            message.channel.sendCode('asciidoc', `== Before Reset == \nComplete Galactic War Battles \nSave Normal Energy
                                  \n== After Reset == \nSpend Normal Energy on Hard Mode Battles`);
             break;
         case 'thu':
         case 'thursday':
-            message.channel.sendCode('asciidoc', `== Before Reset: == \nSpend Normal Energy on Hard Mode Battles \nSave Challenges (Dailies are fine)
+            message.channel.sendCode('asciidoc', `== Before Reset == \nSpend Normal Energy on Hard Mode Battles \nSave Challenges
                                  \n== After Reset == \nComplete Challenges \nSave Normal Energy`);
             break;
         case 'fri':
         case 'friday':
-            message.channel.sendCode('asciidoc', `== Before Reset: == \nComplete Challenges \nSave Normal Energy
+            message.channel.sendCode('asciidoc', `== Before Reset == \nComplete Challenges \nSave Normal Energy
                                  \n== After Reset == \nSpend Normal Energy on Dark Side Battles`);
             break;
         case 'sat':
         case 'saturday':
-            message.channel.sendCode('asciidoc', `== Before Reset: == \nSpend Normal Energy on Dark Side Battles \nSave Arena Battles \nSave Cantina Energy
+            message.channel.sendCode('asciidoc', `== Before Reset == \nSpend Normal Energy on Dark Side Battles \nSave Arena Battles \nSave Cantina Energy
                                  \n== After Reset == \nComplete Arena Battles \nSave Cantina Energy`);
             break;
     }
