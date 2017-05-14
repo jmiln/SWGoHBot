eadee6a126f6dfb0dd743a6eb19102ca048c78b6
JeffreyMilner <j.milner359@gmail.com>
2017-05-13 21:55:27 -0700

    Made it so the bot won't crash if it can't purge (I think)

--- commands/purge.js
+++ commands/purge.js
@@ -1,14 +1,24 @@
 exports.run = function (client, message, args) {
     let messagecount = parseInt(args.join(' '));
-    message.channel.fetchMessages({
-        limit: messagecount
-    }).then(messages => message.channel.bulkDelete(messages));
+    let can_manage_chans = message.channel.permissionsFor(client.user).has('MANAGE_MESSAGES');
+    if(!can_manage_chans) {
+        console.log("Bot does not have the right perms");
+        return;
+    }
+    if (messagecount > 1 && messagecount <= 100) {
+        message.channel.fetchMessages({
+            limit: messagecount
+        }).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+        console.log(message.channel.user + "has purged " + messagecount + " messages.");
+    } else {
+        console.log("You can ony delete between 2 and 100 messages, try again");
+    }
 };
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: [],
+    aliases: ['p'],
     permLevel: 2
 };
 
