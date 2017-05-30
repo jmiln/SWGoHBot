827eb1b01ffd5dcbb34cb4c9217db036e781c674
Jeff <j.milner359@gmail.com>
2017-05-30 15:19:20 +0000

    Err... Now it has the better output

--- commands/mods.js
+++ commands/mods.js
@@ -9,10 +9,18 @@ exports.run = (client, message, args) => {
         var character = charList[ix];
         for(jx = 0; jx < character.aliases.length; jx++) {
             if(searchName.toLowerCase() === character.aliases[jx].toLowerCase()) {
-                if(character.set3 === "") { // If the character only has 2 recommended sets (one is a set of 4)
-                    message.channel.send(" * " + character.name + " * \n### Sets ### \n* " + character.set1 + "\n* " + character.set2 + "\n### Primaries ###" + "\n* Square:   " + character.square + "\n* Arrow:    " + character.arrow + "\n* Diamond:  " + character.diamond + "\n* Triangle: " + character.triangle + "\n* Circle:   " + character.circle + "\n* Cross:    " + character.cross, { code: 'md' });
-                } else {
-                    message.channel.send(" * " + character.name + " * \n### Sets ### \n* " + character.set1 + "\n* " + character.set2 + "\n* " + character.set3 + " \n### Primaries ### \n* Square:   " + character.square + " \n* Arrow:    " + character.arrow + "\n* Diamond:  " + character.diamond + " \n* Triangle: " + character.triangle + " \n* Circle:   " + character.circle + "\n* Cross:    " + character.cross, { code: 'md' });
+                if(message.channel.permissionsFor(client.user).has('EMBED_LINKS')) {    // Check to make sure the bot can post embeds
+                    if(character.set3 === "") { // If the character only has 2 recommended sets (one is a set of 4)
+                        message.channel.send({embed:{ "color": 12339987, "author": { "name": character.name, "url": character.url, "icon_url": character.avatarURL }, "fields": [ { "name": "**### Sets ###**", "value": `* ${character.set1}\n* ${character.set2}` }, { "name": "**### Primaries ###**", "value": `**Square:**      ${character.square}\n**Arrow:**       ${character.arrow}\n**Diamond:**  ${character.diamond}\n**Triangle:**   ${character.triangle}\n**Circle:**        ${character.circle}\n**Cross:**        ${character.cross}` } ] }});
+                    } else {
+                        message.channel.send({embed:{ "color": 12339987, "author": { "name": character.name, "url": character.url, "icon_url": character.avatarURL }, "fields": [ { "name": "**### Sets ###**", "value": `* ${character.set1}\n* ${character.set2}\n* ${character.set3}` }, { "name": "**### Primaries ###**", "value": `**Square:**      ${character.square}\n**Arrow:**       ${character.arrow}\n**Diamond:**  ${character.diamond}\n**Triangle:**   ${character.triangle}\n**Circle:**        ${character.circle}\n**Cross:**        ${character.cross}` } ] }});
+                    }
+                } else {    // Else, just post it as a codeblock
+                    if(character.set3 === "") { // If the character only has 2 recommended sets (one is a set of 4)
+                        message.channel.send(` * ${character.name} * \n### Sets ### \n* ${character.set1} \n* ${character.set2}\n### Primaries ###\n* Square:   ${character.square}\n* Arrow:    ${character.arrow}\n* Diamond:  ${character.diamond}\n* Triangle: ${character.triangle}\n* Circle:   ${character.circle}\n* Cross:    ${character.cross}`, { code: 'md' });
+                    } else {
+                        message.channel.send(` * ${character.name} * \n### Sets ### \n* ${character.set1} \n* ${character.set2}\n* ${character.set3}\n### Primaries ###\n* Square:   ${character.square}\n* Arrow:    ${character.arrow}\n* Diamond:  ${character.diamond}\n* Triangle: ${character.triangle}\n* Circle:   ${character.circle}\n* Cross:    ${character.cross}`, { code: 'md' });
+                    }
                 }
             }
         }
@@ -32,4 +40,3 @@ exports.help = {
     description: 'Shows the mods for a character',
     usage: 'mods [character]'
 };
-
