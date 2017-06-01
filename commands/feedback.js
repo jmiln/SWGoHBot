02c728a5ef3797e9fc4edde74cbe0d99423804f0
Jeff <j.milner359@gmail.com>
2017-06-01 03:35:49 +0000

    Put a confirmation into the feedback command, and made it so the bot's status always shows the help command

--- commands/feedback.js
+++ commands/feedback.js
@@ -3,7 +3,8 @@ const settings = require('../settings.json');
 exports.run = (client, message, args) => {
     const feedback = args.join(" ");
     if(feedback !== "") {  // If there are args/ if they left a message
-        client.channels.get("319362969374949387").send(message.author + " suggests: " + feedback);
+        client.channels.get("319362969374949387").send(message.author + " suggests: \n\`" + feedback + "\`");
+        message.channel.send(`Thanks for your feedback ${message.author.username}`);
     } else {    // If they left no message, grumble at em
         message.reply("Usage is `" + settings.prefix + "feedback [message]`, and requires a message");
     }
--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,6 @@
 const chalk = require('chalk');
+const settings = require("../settings.json");
 module.exports = client => {
     console.log(chalk.bgGreen.black(client.user.username + ' is Online'));
+    client.user.setGame("Say " + settings.prefix + "help");
 };
