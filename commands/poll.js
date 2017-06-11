8a734d627b80214fa4637f444fe15b02e545f648
Jeff <j.milner359@gmail.com>
2017-06-11 19:00:58 +0000

    Added collors depending on ls/ds of the character, and a prototype poll command

---
+++ commands/poll.js
@@ -0,0 +1,27 @@
+exports.run = (client, message) => {
+    // Gets the options
+    let messageArgs = message.content.slice(6);
+    let args = messageArgs.split(",");
+    args = args.map((e) => e.trim());
+
+    console.log(args);
+
+	// const newMsg = await message.channel.send({embed: { "fields" : [ "name": "Poll", "value": `${args[0]}` ]}});
+    // const newMsg = await message.channel.send("blah");
+	// await newMsg.react("blah");
+	// await newMsg.react("blah2");
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 4
+};
+
+exports.help = {
+    name: 'poll',
+    description: 'Starts a poll, with comma separated options',
+    usage: 'poll [Poll Question etc], [option1], [option2], ...'
+};
+
