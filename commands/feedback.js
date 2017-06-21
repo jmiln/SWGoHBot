f55c2fbbbd4fea47a5b477fb2080702789d0d16a
Jeff <j.milner359@gmail.com>
2017-06-21 04:17:48 +0000

    Made the feedback command output more nicely

--- commands/feedback.js
+++ commands/feedback.js
@@ -3,7 +3,16 @@ const settings = require('../settings.json');
 exports.run = (client, message, args) => {
     const feedback = args.join(" ");
     if(feedback !== "") {  // If there are args/ if they left a message
-        client.channels.get("319362969374949387").send(message.author + "(" + message.author.username + ") suggests: \n" + feedback);
+        client.channels.get("319362969374949387").send({embed: {
+            color: 3447003,
+            author: {
+                name: `${message.author.username} (${message.author.id})`,
+                icon_url: message.member.user.avatarURL
+            },
+            title: `${message.guild.name} #${message.channel.name}`,
+            url: '',
+            description: feedback
+        }});
         message.channel.send(`Thanks for your feedback ${message.author.username}`);
     } else {    // If they left no message, grumble at em
         message.reply("Usage is `" + settings.prefix + "feedback [message]`, and requires a message");
