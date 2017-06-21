d403e71e0ce4a71eb4a55ae2a1c3fcd13c73c333
Jeff <j.milner359@gmail.com>
2017-06-21 03:39:48 +0000

    Disabled the joining and leaving messages

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,4 +1,4 @@
-const ddiff = require('return-deep-diff');
+//const ddiff = require('return-deep-diff');
 module.exports = (oMember, nMember) => {
-  console.log(ddiff(oMember, nMember));
+//  console.log(ddiff(oMember, nMember));
 };
