72c909973bda557804ba0862e57d9c1fd9ee4c7a
Jeff <j.milner359@gmail.com>
2017-06-25 07:54:49 +0000

    Added the changelog command

---
+++ commands/changelog.js
@@ -0,0 +1,16 @@
+exports.run = (client, message) => {
+    message.author.send("Use the link below for an invite to the bot's server!\n" + "https://discord.gg/FfwGvhr");
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'changelog',
+    description: 'Sends a link to join the test server where the changelog and announcements about the bot are',
+    usage: 'changelog'
+};
