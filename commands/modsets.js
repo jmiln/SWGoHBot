c5958758e6facea3b2a410a140e9a5f9df03d34d
Jeff <j.milner359@gmail.com>
2017-07-05 06:38:39 +0000

    Added a command to show how many mods you need per set

---
+++ commands/modsets.js
@@ -0,0 +1,17 @@
+exports.run = (client, message) => {
+    message.channel.send(`* Critical Chance:  2\n* Critical Damage:  4\n* Defense:  2\n* Health:   2\n* Offense:  4\n* Potency:  2\n* Speed:    4\n* Tenacity: 2`, {code: 'md'});
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0,
+    type: 'starwars'
+};
+
+exports.help = {
+    name: 'modsets',
+    description: 'Shows how many of each kind of mod you need for a set',
+    usage: 'modsets'
+};
