3bb985132facc3b4ca9a70c904e7cf3025a88ff5
Jeff <j.milner359@gmail.com>
2017-07-21 03:08:45 +0000

    Took the embed away from ;info, since apparently iOS users can not use the links in embeds

--- commands/info.js
+++ commands/info.js
@@ -8,20 +8,21 @@ exports.run = (client, message) => {
     ]
 
 
-    message.channel.send({embed:{ 
-        "color": 547474, 
-        "author": {
-            "name": "Information"
-        },
-        "fields": [{
-            "name": "Links", 
-            "value": "[Join the bot support server here](https://discord.gg/FfwGvhr)\n[Invite the bot with this link](https://discordapp.com/oauth2/authorize?permissions=67624000&scope=bot&client_id=315739499932024834)"
-        },
-        {
-            "name": "Recent Changes",
-            "value": `${changes.join('\n')}`
-        }] 
-    }});
+    // message.channel.send({embed:{ 
+    //     "color": 547474, 
+    //     "author": {
+    //         "name": "Information"
+    //     },
+    //     "fields": [{
+    //         "name": "Links", 
+    //         "value": "Join the bot support server here \nhttps://discord.gg/FfwGvhr\nInvite the bot with this link\nhttps://discordapp.com/oauth2/authorize?permissions=67624000&scope=bot&client_id=315739499932024834"
+    //     },
+    //     {
+    //         "name": "Recent Changes",
+    //         "value": `${changes.join('\n')}`
+    //     }] 
+    // }});
+    message.channel.send(`**### INFORMATION ###** \n**Links**\nJoin the bot support server here \n<https://discord.gg/FfwGvhr>\nInvite the bot with this link\n<https://discordapp.com/oauth2/authorize?permissions=67624000&scope=bot&client_id=315739499932024834>\n\n**Recent Changes**\n${changes.join('\n')}`);
 };
 
 exports.conf = {
