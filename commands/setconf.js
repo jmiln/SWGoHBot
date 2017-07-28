c6f1a618be02feffcd15c698d5ebbb934be296de
JeffreyMilner <j.milner359@gmail.com>
2017-07-28 08:10:31 -0700

    Forgot to change setconf to align with the config changes, should work now.

--- commands/setconf.js
+++ commands/setconf.js
@@ -2,57 +2,53 @@ exports.run = (client, message, args) => {
     const config = client.config;
     const guildSettings = client.guildSettings;
 
-    if(!message.guild) return message.reply(`Sorry, something went wrong, please try again`);
+    if (!message.guild) return message.reply(`Sorry, something went wrong, please try again`);
 
-    const guildConf = guildSettings.get(message.guild.id); 
+    const guildConf = guildSettings.get(message.guild.id);
 
-    if(guildConf) {
-        if(!message.author.id === message.guild.owner.id) {
-            if(!message.member.roles.has(guildConf["adminRole"])) {
+    if (guildConf) {
+        if (message.author.id !== message.guild.owner.id) {
+            if (!message.member.roles.has(guildConf["adminRole"])) {
                 return message.reply(`Sorry, but either you're not an admin, or your server leader has not set up the configs.`);
             }
         }
-        if(!args[0]) return message.reply(`You must select a config option to change.`);
+        if (!args[0]) return message.reply(`You must select a config option to change.`);
         const key = args[0].toLowerCase();
-        
-        if(!args[1]) return message.reply(`You must give a value to change that option to.`);
+
+        if (!args[1]) return message.reply(`You must give a value to change that option to.`);
         const value = args.splice(1).join(" ");
 
-        const onVar =  ["true", "on", "enable"];
+        const onVar = ["true", "on", "enable"];
         const offVar = ["false", "off", "disable"];
 
         // Now we can finally change the value. Here we only have strings for values so we won't
         // bother trying to make sure it's the right type and such. 
         newKey = "";
-        switch(key) {
+        switch (key) {
             case "adminrole":
                 newKey = "adminRole";
-                guildConf["adminRole"] = value;
+                guildConf[newKey] = value;
                 break;
-            case "modrole":
-                newKey = "modRole";
-                guildConf["modRole"] = value;
-                break;
-            case "welcomemessageon":
-                newKey = "welcomeMessageOn";
-                if(onVar.includes(value.toLowerCase())) {
-                    guildConf["welcomeMessageOn"] = true;
-                } else if(offVar.includes(value.toLowerCase())) {
-                    guildConf["welcomeMessageOn"] = false;
+            case "enablewelcome":
+                newKey = "enableWelcome";
+                if (onVar.includes(value.toLowerCase())) {
+                    guildConf[newKey] = true;
+                } else if (offVar.includes(value.toLowerCase())) {
+                    guildConf[newKey] = false;
                 } else {
                     return message.reply(`Invalid value, try true or false`);
                 }
                 break;
             case "welcomemessage":
                 newKey = "welcomeMessage";
-                guildConf["welcomeMessage"] = value;
+                guildConf[newKey] = value;
                 break;
             case "useembeds":
                 newKey = "useEmbeds";
-                if(onVar.includes(value.toLowerCase())) {
-                    guildConf["useEmbeds"] = true;
-                } else if(offVar.includes(value.toLowerCase())) {
-                    guildConf["useEmbeds"] = false;
+                if (onVar.includes(value.toLowerCase())) {
+                    guildConf[newKey] = true;
+                } else if (offVar.includes(value.toLowerCase())) {
+                    guildConf[newKey] = false;
                 } else {
                     return message.reply(`Invalid value, try true or false`);
                 }
@@ -83,6 +79,8 @@ exports.help = {
     category: 'Admin',
     description: 'Used to set the bot\'s config settings.',
     usage: 'setconf [key] [value]',
+    extended: `\`\`\`xl
+        
+        \`\`\``,
     example: 'setconf adminRole Admin'
 };
-
