09f7a51a5d422771f36e929e8d603410851e24cc
JeffreyMilner <j.milner359@gmail.com>
2017-08-02 20:47:58 -0700

    Updated the guildCreate event message for when the bout joins a guild

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -9,5 +9,11 @@ module.exports = (client, guild) => {
     console.log(`I joined ${guild.id}`);
 
     // Messages the guild owner to tell the how to set the bot up
-    guild.owner.send(`Thank you for adding this SWGoHBot! Before using me, please configure the Admin role by running the following command: \`${client.config.prefix}setconf adminRole <AdminRoleName>\`.\nThis will allow anyone with that Discord role to edit the bot's settings.\nAnd now you're all set to begin using SWGoHBot!`);
+    guild.owner.send(`Thank you for adding this SWGoHBot! Before using me, please configure the Admin role by running the following command: 
+                     \n\`${client.config.prefix}setconf adminRole <AdminRoleName>\`.
+                     \nThis will allow anyone with that Discord role to edit the bot's settings. 
+                     \nAlso run \`${client.config.prefix}setconf timezone <timezone>\` to be the timezone of your guild's activities (Default is \`America/Los_Angeles\`),
+                     \nand run \`${client.config.prefix}setconf announceChan <AnnouncementChannel>\` if you want to use the 
+                     \nevents/ announcements and don't want them going off in your default channel.                     
+                     \nAnd now you should be set to begin using SWGoHBot!`);
 };
