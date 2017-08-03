df1d4064363a045c59d712375ba8bc33ebecb0a7
JeffreyMilner <j.milner359@gmail.com>
2017-08-02 20:52:18 -0700

    Updated the updates in the info command

--- commands/info.js
+++ commands/info.js
@@ -4,25 +4,15 @@ exports.run = (client, message) => {
         "Added the **modsets** command",
         "Fixed a bunch of bugs and crashes",
         "Added the **info** command to replace multiple others",
-        "Added the **raidteams** command"
+        "Added the **raidteams** command",
+        "Added timezone and announceChan to the guild configs",
+        "Added the time command to see what time it is in the set timezone",
+        "Added the events command so guilds can set events to alert everyone that's online (Cannot set events in the past)",
+        "Added the faction command, so you can get a list of the characters in a faction",
+        "Added extended help to the editconfs command (DEV-only), and to the setconfs command"
     ]
 
-
-    // message.channel.send({embed:{ 
-    //     "color": 547474, 
-    //     "author": {
-    //         "name": "Information"
-    //     },
-    //     "fields": [{
-    //         "name": "Links", 
-    //         "value": "Join the bot support server here \nhttps://discord.gg/FfwGvhr\nInvite the bot with this link\nhttps://discordapp.com/oauth2/authorize?permissions=67624000&scope=bot&client_id=315739499932024834"
-    //     },
-    //     {
-    //         "name": "Recent Changes",
-    //         "value": `${changes.join('\n')}`
-    //     }] 
-    // }});
-    message.channel.send(`**### INFORMATION ###** \n**Links**\nJoin the bot support server here \n<https://discord.gg/FfwGvhr>\nInvite the bot with this link\n<https://discordapp.com/oauth2/authorize?permissions=67624000&scope=bot&client_id=315739499932024834>\n\n**Recent Changes**\n${changes.join('\n')}`);
+    message.channel.send(`**### INFORMATION ###** \n**Links**\nJoin the bot support server here \n<https://discord.gg/FfwGvhr>\nInvite the bot with this link\n<https://discordapp.com/oauth2/authorize?permissions=67624000&scope=bot&client_id=315739499932024834>\n\n**Recent Changes**\n${changes.slice(Math.max(arr.length - 4, 1)).join('\n')}`);
 };
 
 exports.conf = {
