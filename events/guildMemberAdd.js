4456a720239162acf0d4935fab0ee3efa4f2183f
Jeff <j.milner359@gmail.com>
2017-10-20 02:24:49 +0000

    Missed en enmap relic

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,8 +7,7 @@ module.exports = async (client, member) => {
 
     // Make sure the config option exists. Should not need this, but just in case
     if(!guildConf['announceChan']) {
-        guildConf['announceChan'] = '';
-        client.guildSettings.set(guild.id, guildConf);
+        client.guildSettings.update({announceChan: ''}, {where: {guildID: guild.id}});
     }
 
     // Our welcome message has a bit of a placeholder, let's fix
