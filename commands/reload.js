b3442d394f7aa7f54fe6bc2449404092bd85fb32
Jeff <j.milner359@gmail.com>
2018-05-22 00:24:11 +0000

    Updated reload so it'll show when it's not a real command correctly

--- commands/reload.js
+++ commands/reload.js
@@ -11,10 +11,17 @@ class Reload extends Command {
         });
     }
 
-    async run(client, message, [command]) {
+    async run(client, message, [commandName]) {
+        let command;
+        if (client.commands.has(commandName)) {
+            command = client.commands.get(commandName);
+        } else if (client.aliases.has(commandName)) {
+            command = client.commands.get(client.aliases.get(commandName));
+        }
         if (!command) {
-            return message.channel.send(message.language.get('COMMAND_RELOAD_INVALID_CMD', command)).then(msg => msg.delete(4000)).catch(console.error);
+            return message.channel.send(message.language.get('COMMAND_RELOAD_INVALID_CMD', commandName)).then(msg => msg.delete(4000)).catch(console.error);
         } else {
+            command = command.help.name;
             message.channel.send(`Reloading: ${command}`)
                 .then(async m => {
                     if (client.shard && client.shard.count > 0) {
--- swgohBotShard.js
+++ swgohBotShard.js
@@ -2,6 +2,6 @@
 // This is used for when you get your bot on a ton of servers. 
 
 const Discord = require('discord.js');
-const Manager = new Discord.ShardingManager('./swgohBot.js');
-Manager.spawn(3); 
+const Manager = new Discord.ShardingManager('./swgohBot.js');   // Tell it what the main file we'd otherwise run the bot with is
+Manager.spawn(3);   // Tell it that we want 3 shards (Approx. 900 servers per shard)
 
