efda035985739396b09141f632ca82dac8b60ac2
Jeff <j.milner359@gmail.com>
2018-06-10 16:15:40 +0000

    Quick fix

--- languages/de_DE.js
+++ languages/de_DE.js
@@ -237,7 +237,7 @@ module.exports = class extends Language {
             COMMAND_MYMODS_NO_MODS: (charName) => `Entschuldigung, aber ich konnte keine Mods finden für dein ${charName}`,
             COMMAND_MYMODS_MISSING_MODS: `Entschuldigung, aber ich kann aktuell keine Mods finden. Bitte warte etwas und versuche es erneut.`,
             COMMAND_MYMODS_LAST_UPDATED: (lastUpdated) => `Mods zuletzt aktualisiert: ${lastUpdated}`,
- +            COMMAND_MYMODS_HELP: ({
+            COMMAND_MYMODS_HELP: ({
                 description: "Zeigt die ausgestatteten Mods eines bestimmten Charakters an.",
                 actions: [
                     {
