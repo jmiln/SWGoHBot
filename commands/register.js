d4ad884286bfedf47fb80be75a8110751b388d79
Jeff Milner <j.milner359@gmail.com>
2018-08-07 06:33:25 +0000

    Updated wording

--- commands/register.js
+++ commands/register.js
@@ -94,8 +94,8 @@ class Register extends Command {
                 break;
             case 'update': {
                 return message.channel.send(["Sorry, but this has been disabled.",
-                    "Your profile data will stay the same for an hour after a sync, then will update",
-                    "when you use a command that uses that data after the hour is up"
+                    "Your profile data will stay the same for 2 hours after a sync, then will update",
+                    "when you use a command that uses that data after that time is up"
                 ].join('\n'));
             //     if (!userID || userID === "me") {
             //         userID = message.author.id;
