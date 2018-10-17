28bd30b84d3d75bc4c6226541d380d619d1c382c
Jeff Milner <j.milner359@gmail.com>
2018-10-17 00:40:31 +0000

    Extra check in the squads command in case

--- commands/squads.js
+++ commands/squads.js
@@ -108,6 +108,9 @@ class Squads extends Command {
                     }
                 });
             } else {
+                if (!player || !player.roster) {
+                    return message.channel.send("Sorry, something broke whlie getting your info. Please try again.");
+                }
                 characters.forEach(c => {
                     try {
                         const ch = player.roster.filter(char => char.defId === c.split(":")[0])[0];
