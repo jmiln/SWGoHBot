8f8cbb35bce83a45f290cee4520d31e3a74bbb33
Jeff Milner <j.milner359@gmail.com>
2018-11-27 01:24:26 +0000

    Update the changelog output

--- commands/changelog.js
+++ commands/changelog.js
@@ -14,21 +14,21 @@ class Changelog extends Command {
         logMsg.splice(0, 1);
         logMsg = logMsg.join(" ");
 
-        // If it's set up, send the changelog to a Discord channel
-        if (client.config.changelog.sendChangelogs) {
-            const clMessage = `[${client.myTime()}]\n${logMsg
-                .replace("[Fixed]",   "**[Fixed]**")
-                .replace("[Updated]", "**[Updated]**")
-                .replace("[Added]",   "**[Added]**")
-                .replace("[Removed]", "**[Removed]**")
-                .replace("[Changed]", "**[Changed]**")}`;
-
-            client.sendChangelog(clMessage);
-        }
-
         // Adds it to the db with an auto-incrementing ID
         client.database.models.changelogs.create({
             logText: logMsg
+        }).then(log => {
+            // If it's set up, send the changelog to a Discord channel
+            if (client.config.changelog.sendChangelogs) {
+                const logID = log.dataValues.id;
+                const clMessage = `[${client.myTime()}]\n${logMsg
+                    .replace("[Fixed]",   "**[Fixed]**")
+                    .replace("[Updated]", "**[Updated]**")
+                    .replace("[Added]",   "**[Added]**")
+                    .replace("[Removed]", "**[Removed]**")
+                    .replace("[Changed]", "**[Changed]**")}`;
+                client.sendChangelog(clMessage + "\nAlso listed at swgohbot.com/changelogs/" + logID);
+            }
         });
     }
 }
