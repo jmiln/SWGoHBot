be12be35c781d969abe25f53b2b31f2b3f0d3e29
Jeff Milner <j.milner359@gmail.com>
2018-11-27 01:09:35 +0000

    Updated to save current guildcount for the site

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 /* eslint no-undef: 0 */
+const fs = require("fs");
 module.exports = async client => {
     // Why await here? Because the ready event isn't actually ready, sometimes
     // guild information will come in *after* ready. 1s is plenty, generally,
@@ -11,6 +12,14 @@ module.exports = async client => {
         readyString = `${client.user.username} is ready to serve ${client.users.size} users in ${client.guilds.size} servers. Shard #${client.shard.id}`;
         if (client.shard.id === 0 && client.config.sendStats) {
             require("../modules/botStats.js")(client);
+        }
+        if (client.shard.id === 0) {
+            // Save the bot's current guild count every 5 minutes
+            setInterval(async () => {
+                const guilds = await client.guildCount();
+                fs.writeFileSync("../dashboard/data/guildCount.txt", guilds, "utf8");
+                console.log("Saved " + guilds + " guilds.");
+            }, 5 * 60 * 1000);
         }
     }
     client.log("Ready", readyString);
