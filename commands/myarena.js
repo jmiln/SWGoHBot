60afd75d78344db67b8fc482a5a857585b3a7193
Jeff Milner <j.milner359@gmail.com>
2018-12-12 06:25:22 +0000

    Fixing myarena too

--- commands/myarena.js
+++ commands/myarena.js
@@ -46,11 +46,13 @@ class MyArena extends Command {
 
         if (!options.flags.stats && player.arena.ship.squad && player.arena.ship.squad.length) {
             const sArena = [];
-            player.arena.ship.squad.forEach((ship, ix) => {
-                const thisShip = player.roster.find(s => s.defId === ship.defId);
+            for (let ix = 0; ix < player.arena.ship.squad.length; ix++) {
+                const ship = player.arena.ship.squad[ix];
+                let thisShip = player.roster.find(s => s.defId === ship.defId);
+                thisShip = await client.swgohAPI.langChar(thisShip, message.guildSettings.swgohLanguage);
                 if (thisShip.name && !thisShip.nameKey) thisShip.nameKey = thisShip.name;
                 sArena.push(`\`${sPositions[ix]}\` ${thisShip.nameKey}`);
-            });
+            }
             fields.push({
                 name: message.language.get("COMMAND_MYARENA_FLEET", player.arena.ship.rank),
                 value: sArena.join("\n") + "\n`------------------------------`",
@@ -61,12 +63,14 @@ class MyArena extends Command {
         let desc = "";
         if (!options.flags.stats) {
             const cArena = [];
-            player.arena.char.squad.forEach((char, ix) => {
-                const thisChar = player.roster.find(c => c.defId === char.defId);        // Get the character
+            for (let ix = 0; ix < player.arena.char.squad.length; ix++) {
+                const char = player.arena.char.squad[ix];
+                let thisChar = player.roster.find(c => c.defId === char.defId);        // Get the character
+                thisChar = await client.swgohAPI.langChar(thisChar, message.guildSettings.swgohLanguage);
                 const thisZ = thisChar.skills.filter(s => s.isZeta && s.tier === 8);    // Get the zetas of that character
                 if (thisChar.name && !thisChar.nameKey) thisChar.nameKey = thisChar.name;
                 cArena.push(`\`${positions[ix]}\` ${"z".repeat(thisZ.length)}${thisChar.nameKey}`);
-            });
+            }
             fields.push({
                 name: message.language.get("COMMAND_MYARENA_ARENA", player.arena.char.rank),
                 value: cArena.join("\n") + "\n`------------------------------`",
@@ -84,8 +88,11 @@ class MyArena extends Command {
                 }});
             }
             const chars = [];
-            player.arena.char.squad.forEach((char, ix) => {
-                const thisChar = player.roster.find(c => c.defId === char.defId);        // Get the character
+            // player.arena.char.squad.forEach((char, ix) => {
+            for (let ix = 0; ix < player.arena.char.squad.length; ix++) {
+                const char = player.arena.char.squad[ix];
+                let thisChar = player.roster.find(c => c.defId === char.defId);        // Get the character
+                thisChar = await client.swgohAPI.langChar(thisChar, message.guildSettings.swgohLanguage);
                 const thisCharStats = playerStats.stats.find(c => c.unit.defId === char.defId);        // Get the character
                 const thisZ = thisChar.skills.filter(s => s.isZeta && s.tier === 8);    // Get the zetas of that character
                 if (thisChar.name && !thisChar.nameKey) thisChar.nameKey = thisChar.name;
@@ -100,7 +107,7 @@ class MyArena extends Command {
                     prot: prot,
                     name: cName
                 });
-            });
+            }
             desc = client.makeTable({
                 pos: {value: "", startWith: "`"},
                 speed:{value:  "Spd", startWith: "[", endWith: "|"},
