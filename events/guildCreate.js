d2caeef35c0ea0d90c1407ba9b6d6cbb87eb4762
Jeff Milner <j.milner359@gmail.com>
2019-02-06 19:50:22 +0000

    Bit of cleanup in guildCreate/Delete

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,7 +3,7 @@ module.exports = async (client, guild) => {
     const defSet = client.config.defaultSettings;
 
     // Make sure it's not a server outage that's causing it to show as leaving/ re-joining
-    if(!guild.available) return;
+    if (!guild.available) return;
 
     const exists = await client.database.models.settings.findOne({where: {guildID: guild.id}})
         .then(token => token !== null)
@@ -27,16 +27,6 @@ module.exports = async (client, guild) => {
                 client.log("GuildCreate", `I joined ${guild.name}(${guild.id})`, "Log", "diff", "+");
             })
             .catch(error => { console.log(error, guild.id); });
-        // Messages the guild owner to tell the how to set the bot up  (Missing a ton/ not sure if it's really needed)
-//         try {
-//             guild.owner.send(`Thank you for adding this SWGoHBot! Before using me, please configure the Admin role by running the following command:
-// \`${client.config.prefix}setconf adminRole add <AdminRoleName>\`.
-// This will allow anyone with that Discord role to edit the bot's settings.
-// Also run \`${client.config.prefix}setconf timezone <timezone>\` to be the timezone of your guild's activities (Default is \`America/Los_Angeles\`), and run \`${client.config.prefix}setconf announceChan <AnnouncementChannel>\` if you want to use the events/ announcements or event commands.
-// After that, you should be set to begin using SWGoHBot!`);
-//         } catch (e) {
-//             console.error("Broke trying to send guildCreate message: " + e);
-//         }
     } else {
         // Log that it joined another guild (Again)
         client.log("GuildCreate", `I re-joined ${guild.name}(${guild.id})`, "Log", "diff", "+");
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -6,13 +6,9 @@ module.exports = async (client, guild) => {
     //     .catch(error => { client.log('ERROR',`Broke in guildDelete(settings) ${error}`); });
 
     // Make sure it's not a server outage that's causing it to show as leaving/ re-joining
-    if(!guild.available) return;
+    if (!guild.available) return;
 
     // Log that the bot left
     client.log("GuildDelete", `I left ${guild.name}(${guild.id})`, "Log", "diff", "-");
-
-    // Sets the status as the current server count and help command
-    // const playingString =  `${client.config.prefix}help ~ ${client.guilds.size} servers`;
-    // client.user.setPresence({ game: { name: playingString, type: 0 } }).catch(console.error);
 };
 
