deff1b375f7dbd2d1740bd5d994123c9ab1846c2
Jeff Milner <j.milner359@gmail.com>
2019-03-04 00:58:17 +0000

    Finally fixing the deprecation warning

--- swgohBot.js
+++ swgohBot.js
@@ -46,11 +46,20 @@ client.aliases = new Collection();
 
 client.evCountdowns = {};
 
+const Op = Sequelize.Op;
 client.database = new Sequelize(client.config.database.data, client.config.database.user, client.config.database.pass, {
     host: client.config.database.host,
     dialect: "postgres",
     logging: false,
-    operatorAliases: false
+    operatorsAliases: {
+        $and: Op.and,
+        $or: Op.or,
+        $eq: Op.eq,
+        $gt: Op.gt,
+        $lt: Op.lt,
+        $lte: Op.lte,
+        $like: Op.like
+    }
 });
 
 
