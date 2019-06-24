84618a0e6fa985e1cccf3405b5d50a997e282b30
Jeff Milner <j.milner359@gmail.com>
2019-06-24 16:05:57 -0700

    Updated some missing client bits

--- modules/botStats.js
+++ modules/botStats.js
@@ -1,13 +1,13 @@
 const { post } = require("snekfetch");
 
-module.exports = (client) => {
+module.exports = (Bot, client) => {
     setInterval(updateStats, 30 * 60 * 1000); // Run this every 30 min
 
     async function updateStats() {
         try {
-            const guilds = await client.guildCount();
+            const guilds = await Bot.guildCount();
             await post(`https://botsfordiscord.com/api/bot/${client.user.id}`)
-                .set("Authorization", client.config.b4dToken)
+                .set("Authorization", Bot.config.b4dToken)
                 .send({ server_count: guilds })
                 .catch(err => console.log("Broke trying to update botStats: " + err));
             console.log("Sending guild count to b4d: " + guilds);
