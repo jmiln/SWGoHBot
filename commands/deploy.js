6c01b41c86ac160b0d5a7ccb0ee92d9fe6a16439
Jeff Milner <j.milner359@gmail.com>
2021-08-24 15:52:28 -0700

    Updated to only add what's new, and ignore global commands for now

--- commands/deploy.js
+++ commands/deploy.js
@@ -1,12 +1,14 @@
 // Copied from https://github.com/AnIdiotsGuide/guidebot/blob/class-v13-update/commands/deploy.js
 const Command = require("../base/Command.js");
+const {inspect} = require("util");
 
 class Deploy extends Command {
     constructor(client) {
         super(client, {
             name: "deploy",
             description: "This will deploy all slash commands.",
-            category: "System",
+            category: "Dev",
+            permLevel: 10,
             usage: "deploy",
             aliases: ["d"],
             enabled: true
@@ -14,24 +16,82 @@ class Deploy extends Command {
     }
 
     async run(Bot, message, args, options) { // eslint-disable-line no-unused-vars
-    // Filter the slash commands to find guild only ones.
-        const guildCmds = message.client.slashcmds.filter(c => c.guildOnly).map(c => c.commandData);
+        try {
+            // Filter the slash commands to find guild only ones.
+            const guildCmds = message.client.slashcmds.filter(c => c.guildOnly).map(c => c.commandData);
 
-        // Now we filter out global commands by inverting the filter.
-        const globalCmds = message.client.slashcmds.filter(c => !c.guildOnly).map(c => c.commandData);
+            // The currently deployed commands
+            const currentGuildCommands = await message.client.guilds.cache.get(message.guild.id)?.commands.fetch();
+            const changedGuildComs = [];
+            const newGuildComs = [];
 
-        // Give the user a notification the commands are deploying.
-        await message.channel.send("Deploying commands!");
+            // Work through all the commands that are already deployed, and see which ones have changed
+            guildCmds.forEach(cmd => {
+                const thisCom = currentGuildCommands.find(c => c.name === cmd.name);
+                let isDiff = false;
 
-        // We'll use set but please keep in mind that `set` is overkill for a singular command.
-        // Set the guild commands like this.
-        await message.client.guilds.cache.get(message.guild.id)?.commands.set(guildCmds);
+                // If there's no match, it definitely goes in
+                if (!thisCom) {
+                    console.log("Need to add " + cmd.name);
+                    return newGuildComs.push(cmd);
+                } else {
+                    // Fill in various options info, just in case
+                    for (const ix in cmd.options) {
+                        if (!cmd.options[ix].required) cmd.options[ix].required = false;
+                        if (!cmd.options[ix].choices) cmd.options[ix].choices = undefined;
+                        if (!cmd.options[ix].options) cmd.options[ix].options = undefined;
 
-        // Then set the global commands like this.
-        await message.client.application?.commands.set(globalCmds).catch(e => console.log(e));
+                        for (const op of Object.keys(cmd.options[ix])) {
+                            if (cmd.options[op] !== thisCom.options[op]) {
+                                isDiff = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!cmd.defaultPermission) cmd.defaultPermission = true;
 
-        // Reply to the user that the commands have been deployed.
-        await message.channel.send("All commands deployed!");
+                    if (cmd?.description !== thisCom?.description) { isDiff = true; }
+                    if (cmd?.defaultPermission !== thisCom?.defaultPermission) { isDiff = true; }
+                }
+
+                // If something has changed, stick it in there
+                if (isDiff) {
+                    console.log("Need to update " + thisCom.name);
+                    changedGuildComs.push({id: thisCom.id, com: cmd});
+                }
+            });
+
+            // Now we filter out global commands by inverting the filter.
+            // const globalCmds = message.client.slashcmds.filter(c => !c.guildOnly).map(c => c.commandData);
+
+            // Give the user a notification the commands are deploying.
+            await message.channel.send("Deploying commands!");
+
+            // We'll use set but please keep in mind that `set` is overkill for a singular command.
+            // Set the guild commands like this.
+            // await message.client.guilds.cache.get(message.guild.id)?.commands.set([]);
+            // await message.client.guilds.cache.get(message.guild.id)?.commands.set(guildComsToLoad);
+            if (newGuildComs.length) {
+                for (const newCom of newGuildComs) {
+                    console.log(`Adding ${newCom.name}`);
+                    await message.client.guilds.cache.get(message.guild.id)?.commands.create(newCom);
+                }
+            }
+            if (changedGuildComs.length) {
+                for (const diffCom of changedGuildComs) {
+                    console.log(`Updating ${diffCom.com.name}`);
+                    await message.client.guilds.cache.get(message.guild.id)?.commands.edit(diffCom.id, diffCom.com);
+                }
+            }
+
+            // Then set the global commands like this.
+            // await message.client.application?.commands.set(globalCmds).catch(e => console.log(e));
+
+            // Reply to the user that the commands have been deployed.
+            await message.channel.send("All commands deployed!");
+        } catch (err) {
+            Bot.logger.error(inspect(err, {depth: 5}));
+        }
     }
 }
 
