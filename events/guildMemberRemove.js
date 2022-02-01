6c58532d378bbbaf1a73816825020a6589b2e87a
jmiln <j.milner359@gmail.com>
2022-01-31 16:04:55 -0800

    Cleaned up guildMemberRemove a bit

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,23 +1,18 @@
 const {inspect} = require("util");
 module.exports = async (Bot, client, member) => {
     // This executes when a member joins, so let's welcome them!
-    const guild = member.guild;
-    const guildConf = await Bot.getGuildConf(guild.id);
-
-    // Make sure the config option exists. Should not need this, but just in case
-    if (!guildConf["announceChan"]) {
-        Bot.database.models.settings.update({announceChan: ""}, {where: {guildID: guild.id}});
-    }
+    const guildConf = await Bot.getGuildConf(member.guild.id);
 
     // Our message has a bit of a placeholder, let's fix
-    if (guildConf.enablePart && guildConf.partMessage !== "" && guildConf.announceChan !== "") { // If they have it turned on, and it's not empty
+    if (guildConf.enablePart && !guildConf.partMessage?.length && guildConf.announceChan?.length) { // If they have it turned on, and it's not empty
         const partMessage = guildConf.partMessage
             .replace(/{{user}}/gi,        member.displayName)
             .replace(/{{usermention}}/gi, member.user)
             .replace(/{{server}}/gi,      member.guild.name)
             .replace(/{{prefix}}/gi,      guildConf.prefix);
+
         try {
-            client.announceMsg(guild, partMessage, null, guildConf);
+            await client.announceMsg(member.guild, partMessage, null, guildConf);
         } catch (e) {
             Bot.logger.error(`Error sending partMessage:\nGuildConf:\n${inspect(guildConf)}\n\nError:\n${e}`);
         }
