46ea1470907a62321647031bec3c6f30a218ef61
jmiln <j.milner359@gmail.com>
2022-03-01 17:19:15 -0800

    Remove awaitMessage since I won't be able to see messages (Also interactions make it work)

--- src/modules/functions.ts
+++ src/modules/functions.ts
@@ -469,24 +469,6 @@ module.exports = (Bot, client) => {
         return err;
     };
 
-    /* SINGLE-LINE AWAITMESSAGE
-     * A simple way to grab a single reply, from the user that initiated
-     * the command. Useful to get "precisions" on certain things...
-     * USAGE
-     * const response = await Bot.awaitReply(msg, "Favourite Color?");
-     * msg.reply(`Oh, I really love ${response} too!`);
-     */
-    Bot.awaitReply = async (msg, question, limit = 60000) => {
-        const filter = m => m.author.id === msg.author.id;
-        await msg.channel.send({content: question}).catch(() => {Bot.logger.error("Broke in awaitReply");});
-        try {
-            const collected = await msg.channel.awaitMessages(filter, {max: 1, time: limit, errors: ["time"]});
-            return collected.first().content;
-        } catch (e) {
-            return false;
-        }
-    };
-
     // String Truncate function
     // Bot.truncate = (string, len, terminator="...") => {
     //     const termLength = terminator.length;
