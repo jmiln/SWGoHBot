e4f4ae97955877a35d9ebffbb03e083c9c0c342a
jmiln <j.milner359@gmail.com>
2022-05-16 17:21:16 -0700

    Change up how the emoteIDs are stored

--- src/data/emoteIDs.ts
+++ src/data/emoteIDs.ts
@@ -1,49 +1,48 @@
-const emoteIDs = {
+export const emoteIDs = {
     // Ability materials
-    abilityMatMK3: "351808811213783040",
-    omegaMat:      "351808811356520452",
-    zetaMat:       "351808811817893888",
-    omicronMat:    "912084903036874802",
+    abilityMatMK3: "<:abilityMatMK3:351808811213783040>",
+    omegaMat:      "<:omegaMat:351808811356520452>",
+    zetaMat:       "<:zetaMat:351808811817893888>",
+    omicronMat:    "<:omicronMat:912084903036874802>",
 
     // Base grey mod shapes
-    squareMod:   "362066327101243392",
-    arrowMod:    "362066325474115605",
-    diamondMod:  "362066326925082637",
-    triangleMod: "362066327168352257",
-    circleMod:   "362066326996385812",
-    crossMod:    "362066327516610570",
+    squareMod:   "<:squareMod:362066327101243392>",
+    arrowMod:    "<:arrowMod:362066325474115605>",
+    diamondMod:  "<:diamondMod:362066326925082637>",
+    triangleMod: "<:triangleMod:362066327168352257>",
+    circleMod:   "<:circleMod:362066326996385812>",
+    crossMod:    "<:crossMod:362066327516610570>",
 
     // Base gold mod shapes
-    squareModGold:  "581365987602071552",
-    arrowModGold:   "581365987631431690",
-    diamondModGold: "581365987685695518",
-    triangleModGold:"581365987660791808",
-    circleModGold:  "581365987933290496",
-    crossModGold:   "581365987417522177",
+    squareModGold:  "<:squareModGold:581365987602071552>",
+    arrowModGold:   "<:arrowModGold:581365987631431690>",
+    diamondModGold: "<:diamondModGold:581365987685695518>",
+    triangleModGold:"<:triangleModGold:581365987660791808>",
+    circleModGold:  "<:circleModGold:581365987933290496>",
+    crossModGold:   "<:crossModGold:581365987417522177>",
 
     // Base mod set icons
-    modsetCritChance: "437150252999507968",
-    modsetCritDamage: "437150252928204800",
-    modsetDefense:    "437150252865421312",
-    modsetHealth:     "437150253062422530",
-    modsetOffense:    "437150252944982026",
-    modsetPotency:    "437150252844187648",
-    modsetSpeed:      "437150253117079582",
-    modsetTenacity:   "437150253179863040",
+    modsetCritChance: "<:modsetCritChance:437150252999507968>",
+    modsetCritDamage: "<:modsetCritDamage:437150252928204800>",
+    modsetDefense:    "<:modsetDefense:437150252865421312>",
+    modsetHealth:     "<:modsetHealth:437150253062422530>",
+    modsetOffense:    "<:modsetOffense:437150252944982026>",
+    modsetPotency:    "<:modsetPotency:437150252844187648>",
+    modsetSpeed:      "<:modsetSpeed:437150253117079582>",
+    modsetTenacity:   "<:modsetTenacity:437150253179863040>",
 
     // Stars for x/7 rarities?
-    starActive:   "581365989396971526",
-    starInactive: "581365987807330304",
+    starActive:   "<:starActive:581365989396971526>",
+    starInactive: "<:starInactive:581365987807330304>",
 
     // Currency icons
-    arenaShopCurrency:   "581365988268703744",
-    cantinaShopCurrency: "581365988029759488",
-    creditsCurrency:     "581368131939991552",
-    fleetShopCurrency:   "581365988046405643",
-    guildEventCurrency:  "581365988386144256",
-    guildShopCurrency:   "581365988025696272",
-    gwShopCurrency:      "581365988105125899",
-    shardShopCurrency:   "581365987987816468"
+    arenaShopCurrency:   "<:arenaShopCurrency:581365988268703744>",
+    cantinaShopCurrency: "<:cantinaShopCurrency:581365988029759488>",
+    creditsCurrency:     "<:creditsCurrency:581368131939991552>",
+    fleetShopCurrency:   "<:fleetShopCurrency:581365988046405643>",
+    guildEventCurrency:  "<:guildEventCurrency:581365988386144256>",
+    guildShopCurrency:   "<:guildShopCurrency:581365988025696272>",
+    gwShopCurrency:      "<:gwShopCurrency:581365988105125899>",
+    shardShopCurrency:   "<:shardShopCurrency:581365987987816468>"
 };
 
-export default emoteIDs;
