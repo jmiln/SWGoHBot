d9c16a5304f77848f4b8f2c9344e2f08699ee38e
jmiln <j.milner359@gmail.com>
2022-06-27 17:11:25 -0700

    Update the sharding file to log more stuff

--- swgohBotShard.js
+++ swgohBotShard.js
@@ -5,8 +5,24 @@ const Discord = require("discord.js");
 const Manager = new Discord.ShardingManager("./swgohBot.js",{
     totalShards: config.shardCount  // Tell it how many shards we want (Approx. 1100 servers per shard)
 });
-Manager.spawn();
+
+// Give it a large timeout since it refuses to work otherwise
+Manager.spawn({timeout: 60000});
 
 Manager.on("shardCreate", (shard) => {
-    console.log(`Creating Shard ${shard.id + 1}/${Manager.totalShards}`);
+    shard.on("reconnecting", () => {
+        console.log(`Reconnecting shard: [${shard.id}]`);
+    });
+    shard.on("spawn", () => {
+        console.log(`Spawned shard: [${shard.id}]`);
+    });
+    // shard.on("ready", () => {
+    //     console.log(`Shard [${shard.id}] is ready`);
+    // });
+    shard.on("death", () => {
+        console.log(`Shard Died: [${shard.id}]`);
+    });
+    shard.on("error", (err) => {
+        console.log("ERROR: Shard had issues starting: \n" + err);
+    });
 });
