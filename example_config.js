1fe9375bf7f5df5b48e8391615447011a0290dff
jmiln <j.milner359@gmail.com>
2022-09-19 14:58:59 -0700

    Update the example config to be v14 compliant

--- example_config.js
+++ example_config.js
@@ -1,13 +1,10 @@
-const { Intents } = require("discord.js");
+const { GatewayIntentBits, Partials, ApplicationCommandOptionType } = require("discord.js");
 const config = {
     // The Discord ID for the bot owner (Gets max perms/ can use Dev commands)
     ownerid: "",
 
     // Default server to deploy dev / guild_only commands to
     dev_server: null,
-
-    // The prefix for commands (';' is used as ;help, '+' for +help etc. )
-    prefix: ";",
 
     // The bot's token. Get it from https://discordapp.com/developers/applications/me
     token: "",
@@ -17,14 +14,16 @@ const config = {
 
     // Enable the various intents that are needed
     botIntents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.DIRECT_MESSAGES
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.DirectMessages
     ],
 
     // Partials your bot may need should go here, CHANNEL is required for DM's
-    partials: ["CHANNEL"],
+    partials: [
+        Partials.Channel
+    ],
 
     // Let it register global commands or not
     enableGlobalCmds: true,
@@ -49,70 +48,70 @@ const config = {
     typedDefaultSettings: {
         // Settings in a way that the bot can parse em out into slash command args
         //
-        // Will remove the prefix once it's all converted/ forced to be useless
+        // Removed the prefix now that it should all be slash commands
         //
         adminRole: {
             value: ["Administrator"],
-            type: "ROLE",
+            type: ApplicationCommandOptionType.Role,
             isArray: true,
             description: "A list of the roles that are allowed to mess with settings/ events."
         },
         enableWelcome: {
             value: false,
-            type: "BOOLEAN",
+            type: ApplicationCommandOptionType.Boolean,
             description: "Toggle the welcome message"
         },
         welcomeMessage: {
             value: "Say hello to {{user}}, everyone! We all need a warm welcome sometimes :D",
-            type: "STRING",
+            type: ApplicationCommandOptionType.String,
             description: "Set the welcome message text"
         },
         enablePart: {
             value: false,
-            type: "BOOLEAN",
+            type: ApplicationCommandOptionType.Boolean,
             description: "Toggle the parting/ leaving message"
         },
         partMessage: {
             value: "Goodbye {{user}}, thanks for stopping by!",
-            type: "STRING",
+            type: ApplicationCommandOptionType.String,
             description: "Set the part message text"
         },
         timezone: {
             value: "America/Los_Angeles",
-            type: "STRING",
+            type: ApplicationCommandOptionType.String,
             description: "Set the timezone to be referenced for events and such in the guild"
         },
         announceChan: {
             value: "",
-            type: "CHANNEL",
+            type: ApplicationCommandOptionType.Channel,
             description: "Set the default channel for events to announce to"
         },
         useEventPages: {
             value: false,
-            type: "BOOLEAN",
+            type: ApplicationCommandOptionType.Boolean,
             description: "Set it to show your events list in pages"
         },
         eventCountdown: {
             value: [2880, 1440, 720, 360, 180, 120, 60, 30, 10, 5],
-            type: "INTEGER",
+            type: ApplicationCommandOptionType.Integer,
             isArray: true,
             description: "Set how long before events is should warn you."
         },
         language: {
             value: "en_US",
-            type: "STRING",
+            type: ApplicationCommandOptionType.String,
             description: "Change the language (Limited options outside of English)",
             choices: ["en_US", "de_DE", "es_SP", "ko_KR", "pt_BR"],
         },
         swgohLanguage: {
             value: "ENG_US",
-            type: "STRING",
+            type: ApplicationCommandOptionType.String,
             choices: ["ENG_US", "GER_DE", "SPA_XM", "FRE_FR", "RUS_RU", "POR_BR", "KOR_KR", "ITA_IT", "TUR_TR", "CHS_CN", "CHT_CN", "IND_ID", "JPN_JP", "THA_TH"],
             description: "Change the language of the data from in-game"
         },
         shardtimeVertical: {
             value: false,
-            type: "BOOLEAN",
+            type: ApplicationCommandOptionType.Boolean,
             description: "Display the shardtimes info vertically"
         }
     },
@@ -181,7 +180,7 @@ const config = {
     // The IP/ Port for the bot to use for the image server
     imageServIP_Port: "http://localhost:PORT",
 
-    // Some VIPs that can use patreon perks, put their Discord IDs in the arry
+    // Some VIPs that can use patreon perks, put their Discord IDs in the array
     patrons: [],
     // Need a Patreon account for this
     // patreon: {
