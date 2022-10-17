dfe3325b5ea24bafa85a79da3bcc6fa49a4677af
jmiln <j.milner359@gmail.com>
2022-10-17 14:23:12 -0700

    Fix partMessage not working & added Beskar Mando aliases

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,21 +1,16 @@
 const {inspect} = require("util");
 module.exports = async (Bot, client, member) => {
     // This executes when a member joins, so let's welcome them!
-    const guild = member.guild;
-    if (!guild) {
-        console.log("[guildMemberAdd] Missing guild???");
-        return;
-    }
-    const guildConf = await Bot.getGuildSettings(guild.id);
+    const guildConf = await Bot.getGuildSettings(member.guild.id);
 
-    // Our welcome message has a bit of a placeholder, let's fix
-    if (guildConf.enableWelcome && guildConf.welcomeMessage !== "" && guildConf.announceChan !== "") { // If they have it turned on, and it's not empty
+    if (guildConf.enableWelcome && guildConf.welcomeMessage?.length && guildConf.announceChan?.length) { // If they have it turned on, and it's not empty
         const welcomeMessage = guildConf.welcomeMessage
             .replace(/{{user}}/gi, member.user.username)
             .replace(/{{usermention}}/gi, member.user)
             .replace(/{{server}}/gi, member.guild.name);
+
         try {
-            client.announceMsg(guild, welcomeMessage, null, guildConf);
+            client.announceMsg(member.guild, welcomeMessage, null, guildConf);
         } catch (e) {
             Bot.logger.error(`Error sending welcomeMessage:\n\nGuildConf:\n${inspect(guildConf)}\n\nError:\n${e}`);
         }
