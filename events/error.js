6ad3c5c43e6516b360cc420a6f83cd3321374c6e
jmiln <j.milner359@gmail.com>
2022-10-31 15:49:43 -0700

    Remove the error event since it just spits out undefined

--- events/error.js
+++
@@ -1,8 +0,0 @@
-const {inspect} = require("util");
-module.exports = async (Bot, client, err) => {
-    if (err.error.toString().indexOf("ECONNRESET") > -1) {
-        Bot.logger.error("Connection error");
-    } else {
-        Bot.logger.error("ERROR", inspect(err.error), true);
-    }
-};
