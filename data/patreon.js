ba5e09fd2582777fc2ee372b4b78cb4697cf9ed6
jmiln <j.milner359@gmail.com>
2023-01-24 16:19:43 -0800

    Add a Patreon command & update the data file to support it

---
+++ data/patreon.js
@@ -0,0 +1,40 @@
+// TODO Change it out so there's comments per-unlock, so the later ones
+// override lower tier ones if available. (ArenaAlert/ ArenaWatch)
+module.exports = {
+    tiers: {
+        1: {
+            name: "Youngling",
+            benefits : {
+                ArenaAlert: "Enable the ArenaAlert command to watch your primary registered account for arena drops",
+                ArenaWatch: "Enable ArenaWatch and it's payout monitor for 1 allycode",
+                GuildTickets: "Enable the GuildTickets command & monitor",
+                GuildUpdate: "Enable the GuildUpdate command & monitors"
+            },
+            playerTime: 1,
+            guildTime: 6,
+        },
+        5: {
+            name: "Padawan",
+            benefits: {
+                ArenaAlert: "Enable ArenaAlert to watch all registered accounts for arena drops",
+                ArenaWatch: "Enable ArenaWatch and it's payout monitor for up to 20 allycodes"
+            },
+            playerTime: 1,
+            guildTime: 3,
+        },
+        10: {
+            name: "Jedi Knight",
+            benefits: {
+                ArenaWatch: "Enable ArenaWatch and it's payout monitor for up to 50 allycodes"
+            },
+            playerTime: 1,
+            guildTime: 3,
+        }
+    },
+    commands: {
+        "Arena Alert"  : "Get DMs when your registered accounts' arena rank changes",
+        "Arena Watch"  : "Watch up to 50 ally codes for arena rank changes & payouts, and log them to a channel",
+        "Guild Updates": "The bot will check every hour and log any changes in each member's roster",
+        "Guild Tickets": "Watch each guild member & keep a log of who has not hit the daily 600 tickets",
+    }
+};
---
+++ slash/patreon.js
@@ -0,0 +1,160 @@
+const { ApplicationCommandOptionType } = require("discord.js");
+const Command = require("../base/slashCommand");
+const patreonInfo = require("../data/patreon.js");
+
+class Patreon extends Command {
+    constructor(Bot) {
+        super(Bot, {
+            name: "patreon",
+            enabled: true,
+            guildOnly: false,
+            options: [
+                {
+                    name: "display",
+                    description: "Choose what to show",
+                    type: ApplicationCommandOptionType.String,
+                    required: false,
+                    choices: [
+                        {
+                            name: "Commands",
+                            value: "commands"
+                        },
+                        {
+                            name: "Benefits",
+                            value: "benefits"
+                        },
+                        {
+                            name: "my_info",
+                            value: "my_info"
+                        }
+                    ]
+                }
+            ]
+        });
+    }
+
+    async run(Bot, interaction) {
+        const userID = interaction.user.id;
+        const display = interaction.options.getString("display") || "none";
+        const fields = [];
+        let description = null;
+        let ephemeral = false;
+
+        switch (display) {
+            case "benefits": {
+                // Spit out the benefits data
+                for (const tier of Object.keys(patreonInfo.tiers)) {
+                    const thisTier = patreonInfo.tiers[tier];
+
+                    fields.push({
+                        name: `${thisTier.name} - $${tier}`,
+                        value: [
+                            `>>> Player data updates: **every ${thisTier.playerTime} hours**`,
+                            `Guild data updates: **every ${thisTier.guildTime} hours**`,
+                            "",
+                            `**__Benefits__:**${parseInt(tier, 10) > 1 ? "\nEverything above +" : ""}`,
+                            Object.keys(thisTier.benefits).map(ben => {
+                                return `${ben}:\n${thisTier.benefits[ben]}`;
+                            }).join("\n\n")
+                        ].join("\n")
+                    });
+                }
+                break;
+            }
+            case "commands": {
+                // Spit out the commands data
+                const info = patCmdinfo();
+                fields.push(info);
+                break;
+            }
+            case "my_info":
+            default: {
+                const pat = Bot.getPatronUser(interaction.user.id);
+                ephemeral = true;
+
+                if (!pat || pat.amount_cents < 100) {
+                    // If the user isn't subscribed, say so, provide a link to show more & show command examples
+                    description = "You are not currently subscribed through [Patreon](https://patreon.com/swgohbot)";
+
+                    const info = patCmdinfo();
+                    fields.push(info);
+                } else {
+                    // If they are subscribed, show what they have available
+                    const tierNum = getTier(pat.amount_cents);
+                    const thisTier = patreonInfo.tiers[tierNum];
+
+                    // Player-specific pulls: patreonInfo[tier].playerTime
+                    // Guild-specific pulls: patreonInfo[tier].guildTime
+                    description = `**__${thisTier.name} tier__**:`;
+                    fields.push({
+                        name: "Pull Times",
+                        value: [
+                            `>>> Player data pulls: **2hr** => **${thisTier.playerTime}hr**`,
+                            `Guild data pulls: **6hr** ${thisTier.guildTime < 6 ? `=> **${thisTier.guildTime}hr**` : ""}`
+                        ].join("\n")
+                    });
+
+                    // Benefits: patreonInfo[tier].benefits
+                    //   - For this, it'd be everything prior + each new bit
+                    const tiers = Object.keys(patreonInfo.tiers)
+                        .map(t => parseInt(t, 10))  // Make sure they're numbers instead of strings
+                        .filter(t => t < tierNum)   // Grab just the ones under the current tier
+                        .sort().reverse();          // Sort it backwards so it's highest first
+                    const outObj = {...thisTier.benefits};
+                    for (const t of tiers) {
+                        const thisT = patreonInfo.tiers[t];
+                        for (const benefit of Object.keys(thisT.benefits)) {
+                            if (!outObj[benefit]) {
+                                outObj[benefit] = thisT.benefits[benefit];
+                            }
+                        }
+                    }
+                    fields.push({
+                        name: "Command Benefits",
+                        value: ">>> " + Object.keys(outObj).map(ben => `**${ben}:** ${outObj[ben]}`).join("\n")
+                    });
+                }
+                break;
+            }
+        }
+
+        const u = await interaction.client.users.fetch(userID);
+        return interaction.reply({
+            embeds: [{
+                author: {name: u.username},
+                description: description,
+                fields: fields,
+            }],
+            ephemeral: ephemeral
+        });
+
+    }
+}
+
+function getTier(amount_cents) {
+    const tier100Cent = amount_cents / 100;
+    if (tier100Cent >= 10) {
+        return 10;
+    } else if (tier100Cent >= 5) {
+        return 5;
+    } else {
+        return 1;
+    }
+}
+
+function patCmdinfo() {
+    const patreonValue = [];
+    for (const cmd of Object.keys(patreonInfo.commands)) {
+        patreonValue.push(...[
+            "",
+            `**__${cmd}__**`,
+            patreonInfo.commands[cmd]
+        ]);
+    }
+    return {
+        name: "Patreon Commands",
+        value: ">>> " + patreonValue.join("\n")
+    };
+}
+
+module.exports = Patreon;
