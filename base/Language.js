fe7d103747049d7cf674895797db056913ff0056
jmiln <j.milner359@gmail.com>
2023-04-24 13:13:43 -0700

    Quick fix for broken lang strings

--- base/Language.js
+++ base/Language.js
@@ -1,8 +1,6 @@
-
-
 class Language {
-    constructor(c) {
-        this.Bot = c;
+    constructor(bot) {
+        this.Bot = bot;
     }
 
     get(str, ...args) {
@@ -20,7 +18,18 @@ class Language {
             }
             return res;
         } else {
-            return (args.length > 0 && typeof this.language[str] === "function") ? this.language[str](...args) : this.language[str];
+            let res = null;
+            if (args.length > 0 && typeof this.language[str] === "function") {
+                res = this.language[str](...args);
+            } else {
+                res = this.language[str];
+            }
+            if (!res?.length) {
+                // Something went wonky, and it needs something to be fixed
+                //  - Likely old one where it used to have a prefix / argument, but has been updated to not in the ENG
+                res = "ERROR: Broken string for: " + str;
+            }
+            return res;
         }
     }
 }
