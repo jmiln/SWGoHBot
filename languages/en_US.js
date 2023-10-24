fde1bc3976be5182fa1516c65f6a0411392aa574
jmiln <j.milner359@gmail.com>
2023-10-23 17:19:23 -0700

    Update the polls command to work with the new guild config format

--- languages/en_US.js
+++ languages/en_US.js
@@ -1280,7 +1280,7 @@ module.exports = class extends Language {
             COMMAND_POLL_CHANGED_OPT: (oldOpt, newOpt) => `You have changed your choice from **${oldOpt}** to **${newOpt}**`,
             COMMAND_POLL_REGISTERED: (opt) => `Choice for **${opt}** registered`,
             COMMAND_POLL_CHOICE: (opt, optCount, choice) => `\`[${opt}]\` ${choice}: **${optCount} vote${optCount === 1 ? "" : "s"}**\n`,
-            COMMAND_POLL_FOOTER: (id) => `Poll id: ${id}  -  \`/poll vote <choice>\` to vote`,
+            COMMAND_POLL_FOOTER: (id) => `Use \`/poll vote <choice>\` to vote`,
             COMMAND_POLL_HELP: {
                 description: "Lets you start a poll with multiple options.",
                 actions: [
